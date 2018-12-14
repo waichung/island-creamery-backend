6f37f38c5b71f9bf1a66b62d8d498d9209dd5161
Wai Chung <wai1chung@gmail.com>
2018-12-14 22:22:05 +0800

    Fix dynamic flavor copyright

--- page-ritual.php
+++ page-ritual.php
@@ -7,5 +7,6 @@
 $context = Timber::get_context();
 $context['post'] = new Timber\Post();
 $context['flavours'] = $context['post']->get_field('ritual_flavours');
+$context['flavour'] = substr($_SERVER['REQUEST_URI'], 10);
 
 Timber::render('pages/ritual.twig', $context);
