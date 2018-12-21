62da2ab4f38842a533cc71a2b9a4da0ea40dd897
Wong Wai Chung <wongwaichung@172-7-10-194.lightspeed.chrlnc.sbcglobal.net>
2018-12-21 12:05:07 +0800

    first commit

--- page-home.php
+++ page-home.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Template Name: Ritual Page
+ * Template Name: Home Page
  *
- * Template for the ritual page
+ * Template for the home page
  */
 $context = Timber::get_context();
 $context['post'] = new Timber\Post();
 $context['flavours'] = $context['post']->get_field('ritual_flavours');
 $context['flavour'] = substr($_SERVER['REQUEST_URI'], 10);
 
-Timber::render('pages/ritual.twig', $context);
+Timber::render('pages/home.twig', $context);
